05fddb504de3bdcfa857d9a336526b8b4c26cb64
Andrey Belyakov <rayrah@live.com>
2023-07-12 23:24:39 +0100

    Setup routing and views for detailed product template
    - Update 'purchase' button on product list to
    'buy now'
    - Update link in product list to direct to individual
    products

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
 
+
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries. """
 
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -15,3 +16,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual project details. """
+
+    product = get_object_or_404(Product, pk=product_id)
+    categories = Category.objects.all()
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
