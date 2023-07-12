8ef21ea87b424d1510d32abe2db9e39a8ddc46b7
Andrey Belyakov <rayrah@live.com>
2023-07-12 20:05:56 +0100

    Bug-fix: make category names singular instead of plural

--- products/models.py
+++ products/models.py
@@ -8,12 +8,16 @@ class Category(models.Model):
           verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    singular_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
     def get_friendly_name(self):
         return self.friendly_name
+
+    def get_singular_name(self):
+        return self.singular_name
 
 
 class Product(models.Model):
