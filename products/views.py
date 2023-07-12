dabe20a888b926c3e344240d516658a098318483
Andrey Belyakov <rayrah@live.com>
2023-07-12 18:51:49 +0100

    Pass through categories via `views.py`

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -7,9 +7,11 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
