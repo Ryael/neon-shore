f7ef49fb532dc3728dab2458563ce491f7737385
Andrey Belyakov <rayrah@live.com>
2023-07-16 03:19:35 +0100

    Add scripts to handle adding and removing items in bag

--- static/js/script.js
+++ static/js/script.js
@@ -117,8 +117,8 @@ $('.update-link').click(function(e) {
 $('.remove-item').click(function(e) {
     var csrfToken = "{{ csrf_token }}";
     var itemId = $(this).attr('id').split('remove_')[1];
-    var url = `/bag/remove/${itemId}`;
-    var data = {'csrfmiddlewaretoken': csrfToken, 'size': size};
+    var url = `/bag/remove/${itemId}/`;
+    var data = {'csrfmiddlewaretoken': csrfToken};
 
     $.post(url, data)
      .done(function() {
