3aa592d0b350db0eeea7df0112ec22235eafa54a
Andrey Belyakov <rayrah@live.com>
2023-07-19 05:11:36 +0100

    Setup files for deployment

--- neon_shore/settings.py
+++ neon_shore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-6lwch+(esi$_ozb1ba0kp38h#ffk!ab4!fv#++_z%55x6!pdnv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neon-shore.herokuapp.com']
 
 
 # Application definition
@@ -119,12 +120,17 @@ WSGI_APPLICATION = 'neon_shore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse('postgres://jatdqrhf:1VrNaie15nv-bNPsUThiZQ9fuFvCSkTV@tyke.db.elephantsql.com/jatdqrhf')
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
