bff1df58612815e5b342666249932e1fc411c344
Andrey Belyakov <rayrah@live.com>
2023-08-01 22:10:17 +0100

    Add script for the profile mobile sidebar

--- static/js/script.js
+++ static/js/script.js
@@ -142,3 +142,23 @@ $('.remove-item').click(function(e) {
          location.reload();
      });
 })
+
+/* Profile Sidebar */
+
+window.addEventListener("load", event => {
+
+    // Expand Left Side
+    var icon = document.querySelector('.profile-hamburger'),
+        left = document.querySelector('.profile-navigation');
+
+
+    icon.addEventListener('click', expand);
+
+    function expand() {
+        if (left.classList.contains('show')) {
+            left.classList.remove('show')
+        } else {
+            left.classList.add('show')
+        }
+    }
+});
