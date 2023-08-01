d1770cb2b6b678fcefb620526ff43c2305f9580e
Andrey Belyakov <rayrah@live.com>
2023-08-01 23:11:49 +0100

    Create views and url routing for new profile templates

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('details/', views.profile_details, name='profile_details'),
+    path('orders/', views.profile_orders, name='profile_orders'),
+    path('account/', views.profile_account, name='profile_account')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,43 @@ from .models import UserProfile
 
 
 def profile(request):
-    """ Display the user's profile. """
+    """ Display the user's overall profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+def profile_details(request):
+    """ Display the user's details. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/details.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+def profile_orders(request):
+    """ Display the user's orders. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/orders.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+def profile_account(request):
+    """ Display the user's account information. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/account.html'
     context = {
         'profile': profile,
     }
