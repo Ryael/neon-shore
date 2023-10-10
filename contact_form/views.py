d2d26e6031ad75911d310a3e385ac77db0a90d1e
Andrey Belyakov <rayrah@live.com>
2023-10-10 19:04:36 +0100

    Create views and setup url routing for `contact_form.html`.

--- contact_form/views.py
+++ contact_form/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'We received your query! We will contact you within 48 hours.')
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact_form/contact_form.html', {'form': form})
