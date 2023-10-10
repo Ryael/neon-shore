850eaf03a19b91da99697d87fdcd2e254cf04e5a
Andrey Belyakov <rayrah@live.com>
2023-10-10 19:03:25 +0100

    Create model for the contact form.

--- contact_form/models.py
+++ contact_form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactInquiries(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100)
+    email = models.EmailField(null=False, blank=False, max_length=50)
+    subject = models.CharField(max_length=200)
+    message = models.TextField(null=False, blank=False, max_length=3000)
+    date_received = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" message from {self.email}"
+
+    class Meta:
+        verbose_name_plural = "Contact Inquiries"
