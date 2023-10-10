2d189a3abdac02fe7efde311ca29fa571513dec7
Andrey Belyakov <rayrah@live.com>
2023-10-10 19:03:49 +0100

    Expose contact form to the admin and display its fields.

--- contact_form/admin.py
+++ contact_form/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ContactInquiries
 
 # Register your models here.
+
+class ContactInquiriesAdmin(admin.ModelAdmin):
+    list_display = (
+      'name',
+      'email',
+      'subject',
+      'message',
+      'date_received',
+      )
+    ordering = ('-date_received',)
+
+admin.site.register(ContactInquiries)
