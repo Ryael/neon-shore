492eb0e0c6934f88722b7af8ff5826df74d7852b
Andrey Belyakov <rayrah@live.com>
2023-10-11 04:48:31 +0100

    Add view for all testimonials and route urls to it.

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.testimonial, name='testimonial'),
+    path('all/', views.testimonial_all, name='testimonial_all'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import TestimonialsForm
+from .models import Testimonials
 
 
 def testimonial(request):
     if request.method == 'POST':
         form = TestimonialsForm(request.POST)
         if form.is_valid():
-            form.user=request.user_id
+            form.instance.user = request.user
             form.save()
-            messages.success(request, 'Thank you, we received your testimonial!')
-            return redirect('testimonials')
+            messages.success(request, 'Thank you, we have received your testimonial!')
+            return redirect('all/')
     else:
         form = TestimonialsForm()
     return render(request, 'testimonials/testimonial.html', {'form': form})
+
+
+def testimonial_all(request):
+    testimonials = Testimonials.objects.all().order_by('-id')[:5]
+
+    context = {
+        'testimonials': testimonials,
+    }
+
+    return render(request, 'testimonials/all.html', context)
