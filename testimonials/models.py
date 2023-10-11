d33ce25aa4ffe1e7b86f32c1e64f21ed84a650b6
Andrey Belyakov <rayrah@live.com>
2023-10-11 02:57:12 +0100

    Create testimonials model.

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Testimonials(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_received = models.DateTimeField(auto_now_add=True)
+    review = models.TextField(null=False, blank=False, max_length=1500)
+
+    class Meta:
+        verbose_name_plural = "Testimonials"
