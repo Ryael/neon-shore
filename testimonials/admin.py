dbba88385bfe0e39c342f9dc024e064cfe9ba1f0
Andrey Belyakov <rayrah@live.com>
2023-10-11 04:46:28 +0100

    Expose testimonials to admin view.

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Testimonials
 
 # Register your models here.
+
+class TestimonialsAdmin(admin.ModelAdmin):
+    baseModel = Testimonials
+    list_display = (
+      'user',
+      'review'
+      )
+
+admin.site.register(Testimonials)
+
