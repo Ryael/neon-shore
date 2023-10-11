b2760d61717d7c36634ba6cfd720f78b5e2cb8ce
Andrey Belyakov <rayrah@live.com>
2023-10-11 04:45:48 +0100

    Update testimonials model with str method and change field name.

--- testimonials/models.py
+++ testimonials/models.py
@@ -7,7 +7,10 @@ from django.contrib.auth.models import User
 class Testimonials(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_received = models.DateTimeField(auto_now_add=True)
-    review = models.TextField(null=False, blank=False, max_length=1500)
+    testimonial = models.TextField(null=False, blank=False, max_length=100)
+
+    def __str__(self):
+        return f"Testimonial from {self.user}."
 
     class Meta:
         verbose_name_plural = "Testimonials"
