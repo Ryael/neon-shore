1b2361e93aebd8fd56af557fdfa9ca2b916a7f16
Andrey Belyakov <rayrah@live.com>
2023-10-12 23:05:28 +0100

    Move JS scripts pertaining to home within the home folder.

--- static/js/script.js
+++ static/js/script.js
@@ -1,39 +1,3 @@
-/* Swiper */
-
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".arrow-right",
-    prevEl: ".arrow-left"
-  },
-  effect: "fade",
-  loop: "infinite",
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: "boolean"
-  }
-});
-
-swiper.on("slideChange", function (sld) {
-  tl.seek(0);
-});
-
-/* Painting Animation */
-
-const frames = 20;
-
-gsap.set(".cover img", {
-  maskSize: `${frames * 100}% 100%`
-});
-
-const tl = gsap.timeline({ repeat: 0, yoyo: false });
-
-tl.to(".cover img", {
-  duration: 2.5,
-  maskPosition: `-${(frames - 1) * 100}% 0%`,
-  ease: `steps(${frames - 1})`
-});
-
 /* Mobile Navigation */
 
 const app = (() => {
