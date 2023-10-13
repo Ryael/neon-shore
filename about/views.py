9bd405ed227da514bbe9835f70ac9726aa874d95
Andrey Belyakov <rayrah@live.com>
2023-10-13 02:43:24 +0100

    Fix validation errors in about.

--- about/views.py
+++ about/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def about(request):
     """ A view to return the about page. """
     return render(request, 'about/about.html')
